7e272e3f1c23c12b6b29eb390b77cb9550692813
Xue Zhang <xue.zhang@shopee.com>
2019-12-23 16:06:49 +0800

    Updates

---
+++ precache-manifest.5c464325970cffbc4cae700e369fddf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbdee9b3657fab8610f8df5f39ebfbf4",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "acdc549f673245cf5e45",
+    "url": "/myapp/static/css/main.11725354.chunk.css"
+  },
+  {
+    "revision": "6bb7f684577e9a1e2fb0",
+    "url": "/myapp/static/js/2.d84b7047.chunk.js"
+  },
+  {
+    "revision": "cc7788a8fdb9dc45a08f0313581f5d60",
+    "url": "/myapp/static/js/2.d84b7047.chunk.js.LICENSE"
+  },
+  {
+    "revision": "acdc549f673245cf5e45",
+    "url": "/myapp/static/js/main.5dfa1029.chunk.js"
+  },
+  {
+    "revision": "e655652f13b4fff60f15",
+    "url": "/myapp/static/js/runtime-main.f7ec23d5.js"
+  },
+  {
+    "revision": "2dc0d2b5540dc2fdc61341d91c2468ab",
+    "url": "/myapp/static/media/analytics.2dc0d2b5.png"
+  }
+]);
